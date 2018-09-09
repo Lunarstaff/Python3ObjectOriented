8ed35855eeafcc6a145b9d422757a2db76bd4a23
Lunarstaff <lywtesting@163.com>
2018-09-09 20:49:29 +0800

    20180909 -面向对象编程第5章：管理对象-002
    -这本书先不看了。后面是第6章数据结构了。以后想了解的时候再回头来看。

--- Book_chapters/Chapter_5/textcode/zip_test_v1.py
+++ Book_chapters/Chapter_5/textcode/zip_test_v1.py
@@ -16,3 +16,33 @@ class ZipReplace:
     def _full_filename(self, filename):
         return os.path.join(self.temp_directory, filename)
 
+
+    def zip_find_replace(self):
+        self.unzip_files()
+        self.find_replace()
+        self.zip_files()
+
+    def unzip_files(self):
+        os.mkdir(self.temp_directory)
+        zip = zipfile.ZipFile(self.filename)
+        try:
+            zip.extract(self.temp_directory)
+        finally:
+            zip.close()
+
+    def find_replace(self):
+        for filename in os.listdir(self.temp_directory):
+            with open(self._full_filename(filename)) as file:
+                contentes = file.read()
+            contentes = contentes.replace(self.search_string, self.replace_string)
+            with open(self._full_filename(filename), "w") as file:
+                file.write(contentes)
+
+    def zip_files(self):
+        file = zipfile.ZipFile(self.filename, "w")
+        for filename in os.listdir(self.temp_directory):
+            file.write(self._full_filename(filename), filename)
+        shutil.rmtree(self.temp_directory)
+
+if __name__ == "__main__":
+    ZipReplace(*sys.argv[1:4]).zip_find_replacd()
